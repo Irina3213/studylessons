9fd8d465a979f02debf79a10a9da494e82236b36
Iryna <yeremizina1@gmail.com>
2026-07-02 16:12:42 +0000

    be start files

---
+++ byte-education/bejs/bemodule1.js
@@ -0,0 +1,87 @@
+console.log('Hello world!');
+console.log(1);
+let test = 'Hi!'
+console.log(test);
+
+
+//command git
+//pwd-tekywyu derectoriyu
+//cd-you can change directory
+
+//mkdir -sozdanie papki
+//mkdir test
+
+//touch-sozdat file
+//touch hello.txt
+
+//ls-vivodit vse files v tvoeu directorii
+//ls -1 -vivedet files v stolbik
+//ls -a -vivesti vsefiles directory
+//ls -1a -vivedet vse files directory v stolbik
+//get --version -versiya git
+
+//git init -inicializiroval repozotoriu
+
+//git status - pokazyet status repozitoriya
+
+//git commit -m ''  -sozdaem commit
+
+//git add .  -dobavit vse files, kotorie sywestvyyut v tekyweu directorii
+//git add test.txt -dobavlyaem opredelenniu file
+
+//git diff -pokazivaet raznicy megdy commitami i pokazivaet, chto izmenilos
+
+//git log -istoriya commitov
+
+//git checkout ykaziveem hesh comita - mogem posmotret' commit i chto bilo v nem
+//git checkout master -vozvrashaet na nashy vetky
+//git checkout - vozvrawaet na shag nazad
+
+//nastraivaem okrygenie git
+//git config --global user.name  -napisat' imya, kotorim bydyt podpisani comiti
+//git config --global user.email = -zapisivaem email na kotoriu zaregistrirovan git hub, chob ne bilo problem
+
+//privyajem lokal'niu repozitoriu k ydalennomy
+//git remote add origin adress repozitoriya
+
+//git branch -M main dlya pereimenovke
+
+//zapushit' v git.hub
+//git push - u origin main -takyu zapis' delaete, kogda perviu raz pushite
+//moget zaprosit' parol  i username ot github
+
+//mogno files zabirat' ili clonirovat', ili peretaskivat'
+//clone repozitor:
+//kopiryem ssilky -> git clone ssilka kotoryyu skopirovali->enter
+
+
+//hw 3
+/**
+ * #1 
+ * sprosit' s pomowyu prompt y polzovatelya 2 chisla i zapisat' ih v peremennie.
+ * sravnit' polychenie chisla.
+ * S pomowyu alert vivesti na ekran soobwenie o tom, kakoe chislo bol'we. Esli chisla ravni- soobwit', chto chisla ravni
+ * Naprimer:
+ * -Pol'zovatel' vvel chisla 2 i 5- vivesti na ekran soobwenie: Chislo 5 bol'we,  chem chislo 2
+ * -Pol'zovatel'vvel chisla 10 i 10- vvivesti na ekran soobwenie: Chisla ravni
+ * Podskazka- Vospol'zyutes' yslovnim operatorom if. Dlya togo chtobi vivesti itogovoe soobwenie, vam ponadobitsaa konkatenaciya strok. Esli interesno pochitaute pro sintaksis shablonih strok v js i vospol'zyutes im vmesto konkatenacii
+ * 
+ * #2
+ * Realizovat' kal'kylyator ispol'zyya prompt i alert.
+ * 1. Posledovatel'no sprosit' y pol'zovatelya dva chisla i jelaemoe deustvie.
+ * 2.Deustviya mogyt bit' + - * /
+ * 3.Vipolnit' matematucheskyu operaciyu c polychenimi  znacheniyami
+ * 4.Rezyl'tat pokazat' pol'zovatelyu
+ * Podskazka: Vdannom Zadanii ispol'zyute operator switch case
+ * 
+ * #3
+ * Napishi prostyyu programmy dopyskaposetiteleu na americanskie gorki. Neobhodimo sprosit' y pol'zovatellya vozrast, daleeproanalizirovat' i dat' emy otvet o dopyske na attrakcion
+ * Vozmognie varianti:
+ * -pensioneram (60-80 let) i podrostkam (12-18 let) dostyp razreshen s razresheniya vzroslih. Po etomy esli vozrast posetitelya nahoditsa v ykazannih vozrastnih diapazonah, to nyzno dopolnitel'no sprosit' ego o razreshenii ot vzroslih
+ * -vzroslim vhod razrewen
+ * -detyam i ochen' pogilim lyudyam vhod zaprewen
+ * Podskazka: dlya obrabotki pervogo varianta vi mogete ispol'zovat' yslovniu operator if vnytri drygogo, to est' vospol'zovat'sa vlogenou konstrykcieu
+ */
+
+
+
---
+++ byte-education/bejs/bemodule2.js
@@ -0,0 +1,33 @@
+let firstVariable;
+firstVariable = 'Hello World!';
+console.log(firstVariable);
+
+let userName = 'Nikita';
+console.log(userName);
+
+//lychwe tak ne pisat;
+let width, height, length;
+let maxSpeed = 200;
+ minSpeed = 40;
+
+ //lychwe tal pisat'
+let button = 'button';
+let form;
+let input;
+
+let myHobby = 'fishing';
+console.log(myHobby);
+
+let childAge = 12;
+childAge=13;
+console.log(childAge);
+
+let rectangleWidth = 200;
+let rectangleHeight = rectangleWidth;
+
+//pravil'noe imenovanie peremennih
+let user;
+let userSurname;
+let info1;
+let $user;
+let _log;
