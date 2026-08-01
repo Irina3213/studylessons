0685899d32ed443ca4951f91d0fed323fca52a9f
Iryna Yeremizina <yeremizina1@gmail.com>
2026-08-01 20:09:07 +0300

    finished  joomhost

--- ot0do1/joomhost/js/main.js
+++ ot0do1/joomhost/js/main.js
@@ -5,5 +5,17 @@ $(function () {
     slidesToScroll: 2,
     arrows: false,
     dots: true,
+    responsive: [
+      {
+        breakpoint: 1106,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+  $(".btn__menu").on("click", function () {
+    $(".menu__list").slideToggle();
   });
 });
