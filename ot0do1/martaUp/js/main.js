a72f62fdf92928a79830967dbc592334873cc982
Iryna <yeremizina1@gmail.com>
2026-08-10 02:03:15 +0000

    specialised martaup

--- ot0do1/martaUp/js/main.js
+++ ot0do1/martaUp/js/main.js
@@ -4,5 +4,8 @@ $(function () {
         vertical: true,
         dots: true,
         dotsClass: 'header-dots',
+        autoplay: 2000
     });
 });
+
+// $('.header-slider').slick({ arrows: false, vertical: true, dots: true, dotsClass: 'header-dots', autoplay: 2000 });
