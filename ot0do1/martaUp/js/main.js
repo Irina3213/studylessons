ad40fa86231302296cd666934d213086f824ccfc
Iryna <yeremizina1@gmail.com>
2026-08-13 02:02:31 +0000

    chenge martaup

--- ot0do1/martaUp/js/main.js
+++ ot0do1/martaUp/js/main.js
@@ -1,10 +1,17 @@
 $(function () {
-    $('.header-slider').slick({
-        arrows: false,
-        vertical: true,
-        dots: true,
-        dotsClass: 'header-dots',
-        autoplay: 2000
+    // $('.header-slider').slick({
+    //     arrows: false,
+    //     vertical: true,
+    //     dots: true,
+    //     dotsClass: 'header-dots',
+    //     autoplay: 2000
+    // });
+    $('.header-slider').slick({ 
+        arrows: false, 
+        vertical: true, 
+        dots: true, 
+        dotsClass: 'header-dots', 
+        autoplay: 2000 
     });
 });
 
