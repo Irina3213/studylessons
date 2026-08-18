bf2edcb8f5d871cdc181c734207c2e2b833b2344
Iryna <yeremizina1@gmail.com>
2026-08-18 17:49:48 +0000

    header responce martaup

--- ot0do1/martaUp/js/main.js
+++ ot0do1/martaUp/js/main.js
@@ -13,6 +13,9 @@ $(function () {
         dotsClass: 'header-dots', 
         autoplay: 1000 
     });
+    $('.menu__btn').on('click', function(){
+        $('.menu__list').slideToggle();
+    })
 });
 
 // $('.header-slider').slick({ arrows: false, vertical: true, dots: true, dotsClass: 'header-dots', autoplay: 2000 });
