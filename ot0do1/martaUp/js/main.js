d40a624505e718faeaa059cce8dd2adcc75a8767
Iryna <yeremizina1@gmail.com>
2026-08-19 01:42:28 +0000

    changes

--- ot0do1/martaUp/js/main.js
+++ ot0do1/martaUp/js/main.js
@@ -15,7 +15,7 @@ $(function () {
     });
     $('.menu__btn').on('click', function(){
         $('.menu__list').slideToggle();
-    })
+    });
 });
 
 // $('.header-slider').slick({ arrows: false, vertical: true, dots: true, dotsClass: 'header-dots', autoplay: 2000 });
